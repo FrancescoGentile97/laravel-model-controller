01dbaf830df03f163342326b9f39b41b74938ec1
FrancescoGentile97 <francesco.gentile97@icloud.com>
2023-01-23 17:35:55 +0100

    added foreach e stampate le card del movie come richiesto milestone(7)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,18 @@
 <!-- @extends("layouts.app") -->
 <div class="container">
-        <div class="row">
+    <div class="row gy-4">
             @foreach ($movies as $movie)
-            <div class="col">
-                <div class="card card bg-dark">
-                    <h3 class="card-title">{{$movie->title}}</h3>
-                </div>
+            
+            <div class="col-4">
+                <div class="card p-4 bg-dark">
+                    <div class="card-body">
+                        <h3 class="card-title text-white">{{$movie->title}}</h3>
+                        <p class="card-text text-white">{{$movie["original_title"]}}</p>
+                        <p class="card-text text-white">{{$movie["date"]}}</p>
+                        <p class="card-text text-white">{{$movie["vote"]}}</p>
+                    </div>      
+                </div>   
             </div>
-        </div>
             @endforeach
+    </div>
+</div>
