7dc9b89fbfd27c0a501155eb0691d29ed8a412ee
FrancescoGentile97 <francesco.gentile97@icloud.com>
2023-01-23 16:25:41 +0100

    aggiunto model/controller e db nel file .env e creata la rotta per gestire la "home"

---
+++ app/Http/Controllers/Guest/PageController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Movie;
+
+class PageController extends Controller
+{
+    function home(){
+        $movies = Movie::where("title","original_title","date","vote");
+        dump($movies);
+        return view("home",[
+        "movies" => $movies
+        ]);
+    }
+}
